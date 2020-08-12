8bd27abcd1a030f8737c237d3ef7500ebebeb3aa
Ricardo Torrão <torrao@gmail.com>
2020-08-12 16:16:35 +0100

    update NextJs and React

--- .next/static/tws-3DwmpzRzc_SlnOclr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002F09cd8e43e6d61481041c14be81f75338244926fc.320460a96bf5424cf616.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- .next/static/zUe-83a8Cb7zXH_qfmNo6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002F808f6d36e9a7e2f09b6ea7acd2c451094272794c.320460a96bf5424cf616.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
